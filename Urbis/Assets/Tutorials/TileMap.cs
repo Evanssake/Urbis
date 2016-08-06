00d4fe59d91b517196ac0dfafdc0e8a3423594a9
Alex Evans <ajtevans0@aol.com>
2016-08-06 15:14:35 +0100

    Addig more to tile map scene

--- Urbis/Assets/Tutorials/TileMap.cs
+++ Urbis/Assets/Tutorials/TileMap.cs
@@ -5,10 +5,10 @@ using System.Collections;
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
 public class TileMap : MonoBehaviour {
-
-    public int no_vertices;
-    public int no_triangles;
-    public int no_normals;
+    public int width_x = 100;
+    public int height_z = 50;
+    int depth_y;
+    public float tileSize = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,35 +17,46 @@ public class TileMap : MonoBehaviour {
 
     void BuildMesh()
     {
+        int numTiles = width_x * height_z;
+        int vwidth_x = width_x + 1;
+        int vheight_z = height_z + 1;
+        int numVerts = vwidth_x * vheight_z;
+        int numTris = numTiles * 2;
+
         // Generate the mesh data
-        Vector3[] vertices = new Vector3[4];
-        int[] triangles = new int[2*3];
-        Vector3[] normals = new Vector3[4];
-        Vector2[] uv = new Vector2[4];
+        Vector3[] vertices = new Vector3[numVerts];
+        int[] triangles = new int[numTris*3];
+        Vector3[] normals = new Vector3[numVerts];
+        Vector2[] uv = new Vector2[numVerts];
 
-        vertices[0] = new Vector3(0, 0, 0);
-        vertices[1] = new Vector3(1, 0, 0);
-        vertices[2] = new Vector3(0, 0, -1);
-        vertices[3] = new Vector3(1, 0, -1);
+       int x, z;
+       for(z=0; z<height_z; z++)
+        {
+            for(x=0; x<width_x; x++)
+            {
+                vertices[z * vwidth_x + x] = new Vector3(x*tileSize, 0, z*tileSize);
+                normals[z * vwidth_x + x] = Vector3.up;
+                uv[z * vwidth_x + x] = new Vector2((float)x / vwidth_x, (float)z / vheight_z);
 
-        triangles[0] = 0;
-        triangles[1] = 3;
-        triangles[2] = 2;
+            }
+        }
 
-        triangles[3] = 0;
-        triangles[4] = 1;
-        triangles[5] = 3;
+        for(z=0; z < height_z; z++)
+        {
+            for(x=0; x < width_x; x++)
+            {
+                int squareIndex = z * width_x + x;
+                int triOffset = squareIndex * 6;
+                triangles[triOffset + 0] = z * vwidth_x + x +            0;
+                triangles[triOffset + 1] = z * vwidth_x + x + vwidth_x + 0;
+                triangles[triOffset + 2] = z * vwidth_x + x + vwidth_x + 1;
 
-        normals[0] = Vector3.up;
-        normals[1] = Vector3.up;
-        normals[2] = Vector3.up;
-        normals[3] = Vector3.up;
-
-        uv[0] = new Vector2(0, 1);
-        uv[1] = new Vector2(1, 1);
-        uv[2] = new Vector2(0, 0);
-        uv[3] = new Vector2(1, 0);
-
+                triangles[triOffset + 3] = z * vwidth_x + x +            0;
+                triangles[triOffset + 4] = z * vwidth_x + x + vwidth_x + 1;
+                triangles[triOffset + 5] = z * vwidth_x + x +            1;
+            }
+        }
+        
         // Create a new mesh and populate
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
